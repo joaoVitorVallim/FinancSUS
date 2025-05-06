041f0e0149cdec1a49dd825b3fa76cb7c5b99690
Breno <ltbreno1@gmail.com>
2025-05-06 14:35:45 -0300

    feat: finalizando hero

--- src/components/Apresentacao/Hero.vue
+++ src/components/Apresentacao/Hero.vue
@@ -5,24 +5,31 @@
   
   gsap.registerPlugin(ScrollTrigger);
   
-  const hero = ref(null);
+  const heroTitulo = ref(null);
+  const heroDescricao = ref(null);
   
   onMounted(() => {
-    gsap.from(hero.value, {
+    gsap.from(heroTitulo.value, {
       opacity: 0,
-      y: -300,
+      x: -300,
+      duration: 1,
+    });
+
+    gsap.from(heroDescricao.value,{
+      opacity: 0,
+      x: 300,
       duration: 1,
     });
   });
   </script>
 
 <template>
-    <section class="hero" ref="hero">
+    <section class="hero">
       <div class="hero-container">
-        <div class="hero-titulo-container">
+        <div class="hero-titulo-container" ref="heroTitulo">
           <h1 class="hero-titulo"><img src="../../assets/LogoPequenoEnergia.png" class="hero-logo" alt="Logo Financsus">FinancSUS</h1>
         </div>
-        <div class="hero-descricao-container">
+        <div class="hero-descricao-container" ref="heroDescricao">
           <h1>Uma doação para o mundo</h1>
         </div>
       </div>
@@ -42,7 +49,7 @@
     display: flex;
     justify-content: start;
     align-items: center;
-    margin-left: 8rem;
+    background-color: #f1f1ed;
   }
 
   .hero-titulo-container{
@@ -60,13 +67,19 @@
     animation: fade 4s ease-in-out infinite; 
     margin: 0;
     display: flex;
+    margin-left: 8rem;
   }
 
   .hero-descricao-container{
     display: flex;
     justify-content: center;
-    width: 80%;
+    width: 95%;
     margin-top: 0;
+    background: linear-gradient(300deg, #b4b494, #70705b);
+    background-size: 200% auto;
+    color: transparent;
+    -webkit-background-clip: text; 
+    
   }
 
   .hero-logo{
