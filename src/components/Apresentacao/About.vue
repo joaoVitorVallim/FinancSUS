00dc09c296d9e9f88093ee31fae34430d7c3fb15
Breno <ltbreno1@gmail.com>
2025-05-07 21:10:38 -0300

    feat: iniciando about vue

--- src/components/Apresentacao/About.vue
+++ src/components/Apresentacao/About.vue
@@ -1,9 +1,10 @@
 <template>
     <section class="about">
       <div class="about-container">
-        <h1 class="about-titulo">FinanSUS: energia limpa, financiada por todos.</h1>
+        <h1 class="about-titulo">Energia limpa, financiada por todos.</h1>
         <div class="about-descricao-container">
-          <h1 class="about-descricao">Uma plataforma de crowdfunding sustável que conecta pessoas a projetos de energia limpa. Incentive ideias inovadoras, fortaleça a consicência ambiental e ajude a construir r</h1>
+          <h1 class="about-descricao">Uma plataforma de crowdfunding sustentável que conecta pessoas a projetos de energia limpa.
+Incentive ideias inovadoras, fortaleça a consciência ambiental e ajude a construir um futuro mais verde — um projeto por vez.</h1>
         </div>
       </div>
     </section>
@@ -21,6 +22,35 @@
   </script>
   
   <style scoped>
+    @font-face {
+      font-family: 'CgAthenaeum';
+      src: url(../../assets/CgAthenaeum.ttf) format('truetype'); 
+      font-weight: normal;
+      font-style: normal;
+    }
 
+    .about{
+      height: 100vh;
+      display: flex;
+      justify-content: start;
+      align-items: center;
+      background-color: #f1f1ed;
+      flex-direction: column;
+    }
+
+    .about-titulo{
+      font-family: CgAthenaeum;
+      font-size: clamp(3rem, 5vw, 10rem);
+      background: linear-gradient(175deg, #b4b494, #70705b);
+      background-size: 200% auto;
+      color: transparent;
+      -webkit-background-clip: text; 
+      animation: fade 4s ease-in-out infinite; 
+      margin: 0;
+      display: flex;
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
   </style>
   
