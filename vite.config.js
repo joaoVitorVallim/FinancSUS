87ea6ab754afe7a7a30634738b449fc2abeed056
Vinicius Butrico <butrico0@gmail.com>
2025-06-03 20:03:48 -0300

    fix: corrigir destino de redirecionamento no jsconfig.json e ajustar configuração de build no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -22,5 +22,13 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
-  }
+  },
+  build: {
+    outDir: 'dist',  
+    assetsDir: 'assets', 
+    sourcemap: false,
+  },
+  base: './',
 })
+
+
