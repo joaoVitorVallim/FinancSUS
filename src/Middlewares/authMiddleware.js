81f87e6cd199f5ad434da975cbb9d788086c037a
Vinicius Butrico <butrico0@gmail.com>
2025-06-03 19:44:49 -0300

    feat: ajuste na rota de autenticação para permitir acesso ao endpoint de detalhes da vakinha

--- src/Middlewares/authMiddleware.js
+++ src/Middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const autenticar = async (req, res, next) => {
 
   if(req.path === "/auth/register" || req.path === "/auth/login" 
-    || req.path === "/vakinha/all" || req.path === "/vakinha/getId" 
+    || req.path === "/vakinha/all" || req.path === "/vakinha/:id" 
     || req.path === "/search"){
     return next();
   }
