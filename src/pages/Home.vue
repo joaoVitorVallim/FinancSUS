4bdb8f6ff73d041914e6db90fd8446ee5944ee2b
Vinicius Butrico <butrico0@gmail.com>
2025-06-03 19:47:58 -0300

    fix: corrigir nomenclatura de importações de componentes na página inicial

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <script setup>
-import Hero from '../components/Home/hero.vue'
+import Hero from '../components/Home/Hero.vue'
 import Categorias from '../components/Home/Categoria.vue';
-import ComoFunciona from '../components/Home/comoFunciona.vue'
+import ComoFunciona from '../components/Home/ComoFunciona.vue'
 import Faq from '../components/Home/Faq.vue'
 import Contribuir from '../components/Home/CriarProjeto.vue';
 </script>
