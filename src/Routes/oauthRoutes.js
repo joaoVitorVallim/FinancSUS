9df7a4d08731f4e3601e2d839aae4ec9bcb3ddd8
Vinicius Butrico <butrico0@gmail.com>
2025-06-04 10:24:41 -0300

    feat: adição de middleware CORS à rota '/link' para controle de acesso

--- src/Routes/oauthRoutes.js
+++ src/Routes/oauthRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { oauth, link, refreshToken } from "../Services/oauthService.js";
 const oauthRouter = express.Router();
+import cors from "cors";
 
 oauthRouter.get("/callback", oauth);
 
-oauthRouter.get("/link", link);
+oauthRouter.get("/link", cors(),link);
 
 oauthRouter.get("/token", refreshToken);
 
