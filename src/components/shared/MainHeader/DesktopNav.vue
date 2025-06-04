b7685e355ecd26237402a2b7bf46b3357195094a
Breno <ltbreno1@gmail.com>
2025-06-04 20:26:03 -0300

    feat: arrumando header

--- src/components/shared/MainHeader/DesktopNav.vue
+++ src/components/shared/MainHeader/DesktopNav.vue
@@ -35,7 +35,7 @@
         <a class="navbar-botoes" href="/projetos">Projetos</a>
       </div>
       <div class="componente-navbar-cadastro">
-        <button @click="goToCadastroProjeto()" id="cadastre-projeto" class="componente-navbar-cadastro-a">Cadastre seu projeto!</button>
+        <button @click="goToCadastroProjeto" id="cadastre-projeto" class="componente-navbar-cadastro-a">Cadastre seu projeto!</button>
         <section v-if="user">
           <span class="componente-navbar-cadastro-a">
             <img src="../../../assets/user.png" width="30px" /> Olá, {{ user.name }}
