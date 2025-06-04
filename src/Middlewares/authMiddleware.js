dc22540ac90d1207d4981c92b594602b1e7d702a
Breno <ltbreno1@gmail.com>
2025-06-04 20:01:35 -0300

    ajustando auth

--- src/Middlewares/authMiddleware.js
+++ src/Middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const autenticar = async (req, res, next) => {
 
   if(req.path === "/auth/register" || req.path === "/auth/login" 
-    || req.path === "/vakinha/all" || (req.path.startsWith("/vakinha/") && req.path.endsWith('/image'))
+    || req.path === "/vakinha/all" || (req.path.startsWith("/vakinha/"))
     || req.path === "/search" || req.path.startsWith('/oauth') || req.path.startsWith('/notifications')) {
     return next();
   }
